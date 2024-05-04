769c5231cfcbc0a89ebff19c7e25e61fdb2bd599
Daniel Ramos <danielramosacosta1@gmail.com>
2024-05-04 09:51:46 +0200

    implement it hardcoded

--- src/Board.ts
+++ src/Board.ts
@@ -8,6 +8,6 @@ export class Board {
   }
 
   map(callback: (cell: Cell, neighbors: Cell[]) => Cell): Board {
-    throw new Error("Method not implemented.")
+    return new Board(this.board.map(r => r.map((cell) => false)))
   }
 }
