ed39419b5b2bbf63849ce6e3b0135a397d5ebeae
Adrián García <adriangargar99@gmail.com>
2024-05-04 09:33:54 +0200

    kill all cells in nextGen

--- src/GameOfLife.ts
+++ src/GameOfLife.ts
@@ -1,13 +1,13 @@
-import { Cell } from "./Cell.js";
+import { Cell } from "./Cell.js"
 
 export class GameOfLife {
-  private board: Cell[][];
+  private board: Cell[][]
 
   public constructor(board: boolean[][]) {
-    this.board = board.map(row => row.map(isAlive => Cell.create(isAlive)))
+    this.board = board.map((row) => row.map((isAlive) => Cell.create(isAlive)))
   }
 
   public nextGen(): void {
-
+    this.board = this.board.map((row) => row.map((cell) => Cell.create(false)))
   }
 }
