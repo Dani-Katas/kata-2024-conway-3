021fff4bf9ab859809b4a990b151e7b7693bf362
Daniel Ramos <danielramosacosta1@gmail.com>
2024-05-04 09:59:27 +0200

    send the cell to the given callback

--- src/Board.spec.ts
+++ src/Board.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "vitest"
+import { describe, it, expect, vi } from "vitest"
 import { Board } from "./Board.js"
 import { Cell } from "./Cell.js"
 
@@ -18,6 +18,15 @@ describe("Board", () => {
       const newBoard = board.map(() => Cell.alive())
 
       expect(newBoard).toEqual(new Board([[Cell.alive()]]))
+    })
+
+    it("sends the given cell to the callback", () => {
+      const board = new Board([[Cell.alive()]])
+      const spy = vi.fn()
+
+      board.map((cell) => spy(cell))
+
+      expect(spy).toHaveBeenCalledWith(Cell.alive())
     })
   })
 })
--- src/Board.ts
+++ src/Board.ts
@@ -4,16 +4,18 @@ export class Board {
   private board: Cell[][]
 
   public constructor(board: boolean[][] | Cell[][]) {
-    this.board = board.map((row) => row.map((isAlive) => {
-      if (isAlive instanceof Cell) {
-        return isAlive
-      }
+    this.board = board.map((row) =>
+      row.map((isAlive) => {
+        if (isAlive instanceof Cell) {
+          return isAlive
+        }
 
-      return Cell.create(isAlive);
-    }))
+        return Cell.create(isAlive)
+      }),
+    )
   }
 
   map(callback: (cell: Cell, neighbors: Cell[]) => Cell): Board {
-    return new Board(this.board.map(r => r.map((cell) => callback(Cell.create(false), []))))
+    return new Board(this.board.map((r) => r.map((cell) => callback(cell, []))))
   }
 }
