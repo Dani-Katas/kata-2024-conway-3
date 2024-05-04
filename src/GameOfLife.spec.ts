f5319fe6cd993ed8c26919807cdda58d164da818
Adrián García <adriangargar99@gmail.com>
2024-05-04 09:34:27 +0200

    delete triangulation test

--- src/GameOfLife.spec.ts
+++ src/GameOfLife.spec.ts
@@ -1,36 +1,22 @@
 import { describe, it, expect } from "vitest"
-import { GameOfLife } from "./GameOfLife.js";
+import { GameOfLife } from "./GameOfLife.js"
 
 describe("GameOfLife", () => {
   it("runs an iteration on a simple board", () => {
     const gameOfLife = new GameOfLife([
       [false, false, false],
       [false, true, false],
-      [false, false, false]
+      [false, false, false],
     ])
 
     gameOfLife.nextGen()
 
-    expect(gameOfLife).toEqual(new GameOfLife([
-      [false, false, false],
-      [false, false, false],
-      [false, false, false]
-    ]))
-  })
-
-  it("runs an iteration on a simple board 2", () => {
-    const gameOfLife = new GameOfLife([
-      [false, false, false],
-      [false, true, false],
-      [false, false, false]
-    ])
-
-    gameOfLife.nextGen()
-
-    expect(gameOfLife).not.toEqual(new GameOfLife([
-      [false, false, false],
-      [false, true, false],
-      [false, false, false]
-    ]))
+    expect(gameOfLife).toEqual(
+      new GameOfLife([
+        [false, false, false],
+        [false, false, false],
+        [false, false, false],
+      ]),
+    )
   })
 })
