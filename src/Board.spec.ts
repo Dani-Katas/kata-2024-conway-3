832cb4c050ec27c41422c41947b6d9846331b908
Daniel Ramos <danielramosacosta1@gmail.com>
2024-05-04 09:56:43 +0200

    organize tests

--- src/Board.spec.ts
+++ src/Board.spec.ts
@@ -3,19 +3,21 @@ import { Board } from "./Board.js"
 import { Cell } from "./Cell.js"
 
 describe("Board", () => {
-  it("can map each cell", () => {
-    const board = new Board([[Cell.alive()]])
+  describe("map", () => {
+    it("can map each cell", () => {
+      const board = new Board([[Cell.alive()]])
 
-    const newBoard = board.map(() => Cell.dead())
+      const newBoard = board.map(() => Cell.dead())
 
-    expect(newBoard).toEqual(new Board([[Cell.dead()]]))
-  })
+      expect(newBoard).toEqual(new Board([[Cell.dead()]]))
+    })
 
-  it("map method calls the callback", () => {
-    const board = new Board([[Cell.dead()]])
+    it("map method calls the callback", () => {
+      const board = new Board([[Cell.dead()]])
 
-    const newBoard = board.map(() => Cell.alive())
+      const newBoard = board.map(() => Cell.alive())
 
-    expect(newBoard).toEqual(new Board([[Cell.alive()]]))
+      expect(newBoard).toEqual(new Board([[Cell.alive()]]))
+    })
   })
 })
