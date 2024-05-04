cbc092045d56f297d2f72d8fa2919e106735725c
Daniel Ramos <danielramosacosta1@gmail.com>
2024-05-04 09:52:34 +0200

    overload constructor

--- src/Board.ts
+++ src/Board.ts
@@ -3,8 +3,14 @@ import { Cell } from "./Cell.js"
 export class Board {
   private board: Cell[][]
 
-  public constructor(board: boolean[][]) {
-    this.board = board.map((row) => row.map((isAlive) => Cell.create(isAlive)))
+  public constructor(board: boolean[][] | Cell[][]) {
+    this.board = board.map((row) => row.map((isAlive) => {
+      if (isAlive instanceof Cell) {
+        return isAlive
+      }
+
+      return Cell.create(isAlive);
+    }))
   }
 
   map(callback: (cell: Cell, neighbors: Cell[]) => Cell): Board {
